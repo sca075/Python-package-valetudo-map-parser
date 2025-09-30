6f8453d440693b9835d53de317e72b55f69ce763
SCA075 <82227818+sca075@users.noreply.github.com>
2025-09-30 13:23:00 +0200

    Add docstrings to methods in shared.py
    Added docstrings for methods in shared.py to improve code documentation and clarity.

--- SCR/valetudo_map_parser/config/shared.py
+++ SCR/valetudo_map_parser/config/shared.py
@@ -147,29 +147,37 @@ class CameraShared:
         return obstacle_links
 
     def update_user_colors(self, user_colors):
+        """Update user colors pallete"""
         self.user_colors = user_colors
 
     def get_user_colors(self):
+        """Return user colors"""
         return self.user_colors
 
     def update_rooms_colors(self, user_colors):
+        """Rooms Colors Update"""
         self.rooms_colors = user_colors
 
     def get_rooms_colors(self):
+        """Return rooms colours"""
         return self.rooms_colors
 
     def reset_trims(self) -> dict:
+        """Reset Cropping trims"""
         self.trims = TrimsData.from_dict(DEFAULT_VALUES["trims_data"])
         return self.trims
 
     async def batch_update(self, **kwargs):
+        """Update the data of Shared in Batch"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     async def batch_get(self, *args):
+        """Get all data from Shared"""
         return {key: getattr(self, key) for key in args}
 
     def generate_attributes(self) -> dict:
+        """Generate entity attributes"""
         attrs = {
             ATTR_CAMERA_MODE: self.camera_mode,
             ATTR_VACUUM_BATTERY: f"{self.vacuum_battery}%",
@@ -206,7 +214,6 @@ class CameraShared:
                 "pil_last_image": self.last_image,
                 "size": self.image_size,
                 "format": self.image_format,
-                "robot_size": self.robot_size,
                 "updated": self.image_last_updated,
             },
             "attributes": self.generate_attributes(),
@@ -214,20 +221,114 @@ class CameraShared:
 
 
 class CameraSharedManager:
-    """Thread-safe manager for CameraShared."""
+    """Camera Shared Manager class."""
 
-    _instance: CameraShared | None = None
-    _lock = asyncio.Lock()
-
-    def __init__(self, file_name: str):
+    def __init__(self, file_name: str, device_info: dict = None):
+        self._instances = {}
+        self._lock = asyncio.Lock()
         self.file_name = file_name
+        if device_info:
+            self.device_info = device_info
+            self.update_shared_data(device_info)
 
-    def get_instance(self) -> CameraShared:
-        if not CameraSharedManager._instance:
-            CameraSharedManager._instance = CameraShared(self.file_name)
-        return CameraSharedManager._instance
+        # Automatically initialize shared data for the instance
+        # self._init_shared_data(device_info)
 
-    def get_data(self) -> dict:
-        """Return the shared data dictionary for the component."""
-        instance = self.get_instance()
-        return instance.to_dict()
+    def update_shared_data(self, device_info):
+        """Initialize the shared data with device_info."""
+        instance = self.get_instance()  # Retrieve the correct instance
+
+        try:
+            # Store the device_info in the instance
+            instance.device_info = device_info
+            _LOGGER.info(
+                "%s: Stored device_info in shared instance", instance.file_name
+            )
+
+            instance.attr_calibration_points = None
+
+            # Initialize shared data with defaults from DEFAULT_VALUES
+            instance.offset_top = device_info.get(
+                CONF_OFFSET_TOP, DEFAULT_VALUES["offset_top"]
+            )
+            instance.offset_down = device_info.get(
+                CONF_OFFSET_BOTTOM, DEFAULT_VALUES["offset_bottom"]
+            )
+            instance.offset_left = device_info.get(
+                CONF_OFFSET_LEFT, DEFAULT_VALUES["offset_left"]
+            )
+            instance.offset_right = device_info.get(
+                CONF_OFFSET_RIGHT, DEFAULT_VALUES["offset_right"]
+            )
+            instance.image_auto_zoom = device_info.get(
+                CONF_AUTO_ZOOM, DEFAULT_VALUES["auto_zoom"]
+            )
+            instance.image_zoom_lock_ratio = device_info.get(
+                CONF_ZOOM_LOCK_RATIO, DEFAULT_VALUES["zoom_lock_ratio"]
+            )
+            instance.image_aspect_ratio = device_info.get(
+                CONF_ASPECT_RATIO, DEFAULT_VALUES["aspect_ratio"]
+            )
+            instance.image_rotate = int(
+                device_info.get(ATTR_ROTATE, DEFAULT_VALUES["rotate_image"])
+            )
+            instance.margins = int(
+                device_info.get(ATTR_MARGINS, DEFAULT_VALUES["margins"])
+            )
+            instance.show_vacuum_state = device_info.get(
+                CONF_VAC_STAT, DEFAULT_VALUES["show_vac_status"]
+            )
+            instance.vacuum_status_font = device_info.get(
+                CONF_VAC_STAT_FONT, DEFAULT_VALUES["vac_status_font"]
+            )
+            instance.vacuum_status_size = device_info.get(
+                CONF_VAC_STAT_SIZE, DEFAULT_VALUES["vac_status_size"]
+            )
+            instance.vacuum_status_position = device_info.get(
+                CONF_VAC_STAT_POS, DEFAULT_VALUES["vac_status_position"]
+            )
+            # If enable_snapshots, check for png in www.
+            instance.enable_snapshots = device_info.get(
+                CONF_SNAPSHOTS_ENABLE, DEFAULT_VALUES["enable_www_snapshots"]
+            )
+            # Ensure trims are updated correctly
+            trim_data = device_info.get("trims_data", DEFAULT_VALUES["trims_data"])
+            instance.trims = TrimsData.from_dict(trim_data)
+            # Robot size
+            robot_size = device_info.get("robot_size", 25)
+            try:
+                robot_size = int(robot_size)
+            except (ValueError, TypeError):
+                robot_size = 25
+            # Clamp robot_size to [8, 25]
+            if robot_size < 8:
+                robot_size = 8
+            elif robot_size > 25:
+                robot_size = 25
+            instance.robot_size = robot_size
+
+        except TypeError as ex:
+            _LOGGER.warning(
+                "Shared data can't be initialized due to a TypeError! %s", ex
+            )
+        except AttributeError as ex:
+            _LOGGER.warning(
+                "Shared data can't be initialized due to an AttributeError! %s", ex
+            )
+        except RuntimeError as ex:
+            _LOGGER.warning(
+                "An unexpected error occurred while initializing shared data %s:", ex
+            )
+
+    def get_instance(self):
+        """Get the shared instance."""
+        if self.file_name not in self._instances:
+            self._instances[self.file_name] = CameraShared(self.file_name)
+            self._instances[self.file_name].file_name = self.file_name
+        return self._instances[self.file_name]
+
+    async def update_instance(self, **kwargs):
+        """Update the shared instance."""
+        async with self._lock:
+            instance = self.get_instance()
+            await instance.batch_update(**kwargs)
